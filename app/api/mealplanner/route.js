c1f636e17f038f14f2fbcde4140d029e8aa9b677
Yash Vardhan Ruia <ruiayashvardhan@gmail.com>
2025-02-23 20:09:02 +0530

    fix: update supabase client import paths in meal planner and tasks routes

--- app/api/mealplanner/route.js
+++ app/api/mealplanner/route.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../../../lib/supabaseClient";
+import { supabase } from "../../../lib/supabaseClient";
 import { v4 as uuidv4 } from "uuid";
 
 // GET the most recent meal plan for a user
